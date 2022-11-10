bc54b4008d491c0b232d9f521c4d6e0881cacaf5
Cristhian Traversi <crist.trav@gmail.com>
2022-11-10 11:57:58 -0300

    Tarea 5 Angular (Parte 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +6,5 @@ import { MenuItem, PrimeIcons } from 'primeng/api';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  items: MenuItem[] = [
-    {
-      label: "Biblioteca",
-      icon: PrimeIcons.BOOK,
-      items: [
-        {
-          label: "Libros",
-          icon: PrimeIcons.BOOKMARK,
-          routerLink: ['/libros']
-        },
-        {
-          label: "Autores",
-          icon: PrimeIcons.USERS,
-          routerLink: ['/autores']
-        }
-      ]
-    }
-  ];
 
 }
